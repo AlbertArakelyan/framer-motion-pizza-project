c6472114fd879e3528db063d4c698ccf75e07fcb
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-03-31 16:44:11 +0400

    keyframes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: '0 0 8px rgb(255, 255, 255)',
+    boxShadow: '0 0 8px rgb(255, 255, 255)',
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -15,12 +23,9 @@ const Home = () => {
       </h2>
       <Link to="/base">
         <motion.button
-          animate={{}}
-          whileHover={{
-            scale: 1.1,
-            textShadow: '0 0 8px rgb(255, 255, 255)',
-            boxShadow: '0 0 8px rgb(255, 255, 255)',
-          }}
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
         >
           Create Your Pizza
         </motion.button>
